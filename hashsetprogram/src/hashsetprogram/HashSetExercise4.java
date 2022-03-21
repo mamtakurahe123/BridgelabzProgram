6d8c697323ba9f5dc88d3a6887e132c0c6620041
mamta maskare <mamtakurahe@gmail.com>
2022-03-21 16:02:20 +0530

    hash set program has been commited

---
+++ hashsetprogram/src/hashsetprogram/HashSetExercise4.java
@@ -0,0 +1,24 @@
+package hashsetprogram;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetExercise4 {
+	
+	public static void main(String[] args) {
+        // Create a empty hash set
+    HashSet<String> h_set = new HashSet<String>();
+  // use add() method to add values in the hash set
+         h_set.add("Red");
+         h_set.add("Green");
+         h_set.add("Black");
+         h_set.add("White");
+         h_set.add("Pink");
+         h_set.add("Yellow");
+   System.out.println("Original Hash Set: " + h_set);
+   // Remove all elements
+   h_set.removeAll(h_set);
+   System.out.println("Hash Set after removing all the elements "+h_set);   
+  }
+
+}
---
+++ hashsetprogram/src/hashsetprogram/HashSetExercise6.java
@@ -0,0 +1,25 @@
+package hashsetprogram;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetExercise6 {
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+        // Create a empty hash set
+    HashSet<String> h_set = new HashSet<String>();
+  // use add() method to add values in the hash set
+         h_set.add("Red");
+         h_set.add("Green");
+         h_set.add("Black");
+         h_set.add("White");
+         h_set.add("Pink");
+         h_set.add("Yellow");
+   System.out.println("Original Hash Set: " + h_set);
+   HashSet <String> new_h_set = new HashSet <String> ();
+         new_h_set = (HashSet<String>)h_set.clone();
+         System.out.println("Cloned Hash Set: " + new_h_set);         
+  }
+
+}
